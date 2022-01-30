787be17b2c32cc8f7d1d51286417db941ff22240
Dutchak Bohdan <Dutchak.Bohdan@gmail.com>
2022-01-30 16:23:06 +0200

    Added models

---
+++ bART/Models/Account.cs
@@ -0,0 +1,8 @@
+﻿namespace bART.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ bART/Models/Incident.cs
@@ -0,0 +1,8 @@
+﻿namespace bART.Models
+{
+    public class Incident
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
